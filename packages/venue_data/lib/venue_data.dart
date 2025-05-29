adfb53b04ea4a9ec5632cb2fcc1588627b4ad7bb
Ahmad Al-Attal <ahmad.alattal@gmail.com>
2025-05-29 22:40:11 +0400

    Add venue_data.dart to export model, mapper, and parser files

---
+++ packages/venue_data/lib/venue_data.dart
@@ -0,0 +1,20 @@
+export 'src/model/venue_dto.dart';
+export 'src/model/category_dto.dart';
+export 'src/model/coordinates_dto.dart';
+export 'src/model/opening_hours_dto.dart';
+export 'src/model/thing_to_do_dto.dart';
+export 'src/model/venue_image_dto.dart';
+
+export 'src/mapper/venue_mapper.dart';
+export 'src/mapper/category_mapper.dart';
+export 'src/mapper/coordinates_mapper.dart';
+export 'src/mapper/opening_hours_mapper.dart';
+export 'src/mapper/thing_to_do_mapper.dart';
+export 'src/mapper/venue_image_mapper.dart';
+
+export 'src/parser/venue_parser.dart';
+export 'src/parser/category_parser.dart';
+export 'src/parser/coordinates_parser.dart';
+export 'src/parser/opening_hours_parser.dart';
+export 'src/parser/thing_to_do_parser.dart';
+export 'src/parser/venue_image_parser.dart';
