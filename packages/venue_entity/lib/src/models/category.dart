760cf753a6ee9450f2bdbcebb89a006eedb0e1e9
Ahmad Al-Attal <ahmad.alattal@gmail.com>
2025-05-29 17:10:25 +0400

    Category class extends Equatable for improved equality comparison

--- packages/venue_entity/lib/src/models/category.dart
+++ packages/venue_entity/lib/src/models/category.dart
@@ -1,5 +1,10 @@
-class Category {
+import 'package:equatable/equatable.dart';
+
+class Category extends Equatable {
   final String name;
 
   Category({required this.name});
+  
+  @override
+  List<Object?> get props => [name];
 }
