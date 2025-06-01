ae68c32a47e22380757dbc9a3badccd41d1c6485
Ahmad Al-Attal <ahmad.alattal@gmail.com>
2025-06-02 01:21:26 +0400

    refactor: Replace GridView with CustomScrollView for improved layout flexibility in VenueGrid widget and add title

--- packages/venue_list/lib/src/presentation/widgets/venue_grid.dart
+++ packages/venue_list/lib/src/presentation/widgets/venue_grid.dart
@@ -8,18 +8,32 @@ class VenueGrid extends StatelessWidget {
   const VenueGrid({super.key, required this.venueGridDisplayModel});
 
   @override
-  Widget build(BuildContext context) => GridView.builder(
-    padding: const EdgeInsets.all(16),
-    itemCount: venueGridDisplayModel.items.length,
-    gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-      maxCrossAxisExtent: 280,
-      mainAxisSpacing: 16,
-      crossAxisSpacing: 16,
-      childAspectRatio: 3 / 3.7,
-    ),
-    itemBuilder: (_, index) {
-      final venue = venueGridDisplayModel.items[index];
-      return VenueCard(model: venue);
-    },
+  Widget build(BuildContext context) => CustomScrollView(
+    slivers: [
+      SliverToBoxAdapter(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Text(
+            venueGridDisplayModel.title,
+            style: Theme.of(context).textTheme.headlineSmall,
+          ),
+        ),
+      ),
+      SliverPadding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        sliver: SliverGrid(
+          delegate: SliverChildBuilderDelegate((_, index) {
+            final venue = venueGridDisplayModel.items[index];
+            return VenueCard(model: venue);
+          }, childCount: venueGridDisplayModel.items.length),
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 280,
+            mainAxisSpacing: 16,
+            crossAxisSpacing: 16,
+            childAspectRatio: 3 / 3.7,
+          ),
+        ),
+      ),
+    ],
   );
 }
