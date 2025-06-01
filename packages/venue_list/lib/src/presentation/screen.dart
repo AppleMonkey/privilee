1a42fbf621a30650ab93d544e441fb296e2ed4ed
Ahmad Al-Attal <ahmad.alattal@gmail.com>
2025-06-02 01:26:26 +0400

    feat: Add Privilee logo SVG asset and update pubspec for asset inclusion

--- packages/venue_list/lib/src/presentation/screen.dart
+++ packages/venue_list/lib/src/presentation/screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/svg.dart' show SvgPicture;
 import 'package:venue_list/src/bloc/venue_list_bloc.dart';
 import 'package:venue_list/src/bloc/venue_list_state.dart';
 import 'package:venue_list/src/presentation/widgets/venue_grid.dart';
@@ -8,23 +9,30 @@ class Screen extends StatelessWidget {
   const Screen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Venues')),
-      body: BlocBuilder<VenueListBloc, VenueListState>(
-        builder: (context, state) {
-          if (state is LoadingState) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (state is LoadedState) {
-            final displayModel = state.venueGrid;
-            return VenueGrid(venueGridDisplayModel: displayModel);
-          } else if (state is ErrorState) {
-            return Center(child: Text('Error: ${state.message}'));
-          } else {
-            return const Center(child: Text('No venues found.'));
-          }
-        },
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(
+      title: SvgPicture.asset(
+        '/assets/privilee_logo.svg',
+        package: 'venue_list',
+        height: 32,
       ),
-    );
-  }
+      centerTitle: true,
+      backgroundColor: Color.fromRGBO(234, 235, 236, 1.0),
+      elevation: 1.0,
+    ),
+    body: BlocBuilder<VenueListBloc, VenueListState>(
+      builder: (context, state) {
+        if (state is LoadingState) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (state is LoadedState) {
+          final displayModel = state.venueGrid;
+          return VenueGrid(venueGridDisplayModel: displayModel);
+        } else if (state is ErrorState) {
+          return Center(child: Text('Error: ${state.message}'));
+        } else {
+          return const Center(child: Text('No venues found.'));
+        }
+      },
+    ),
+  );
 }
